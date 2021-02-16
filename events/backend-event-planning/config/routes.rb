ca84bfc09c8f642a6d29dfee8abb526d7772b325
akrama23 <arundelfootball23@gmail.com>
2021-02-16 11:28:34 -0500

    some auth

--- events/backend-event-planning/config/routes.rb
+++ events/backend-event-planning/config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
 
-      
-      post '/login', to: 'auth#log_in'
+      resources :users
+      resources :events
+      resources :user_events
+      resources :guests
+
+      # post '/login', to: 'auth#log_in'
+      get '/login', to: "auth#login"
+      post '/register', to: "auth#create"
+      get '/logout', to: "auth#logout"
 
     end 
   end 
--- events/client/src/App.js
+++ events/client/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About /About";
+import Home from "./components/Home/Home";
+import Users from "./components/Users/Users";
+import Login from "./components/Login/Login"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About/>
+            </Route>
+            <Route path="/users">
+              <Users/>
+            </Route>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+          
+  }
+
 }
 
-export default App;
